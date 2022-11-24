ea7231893e16fbbe9b21231fa5a428f814dbf5b2
pintoflager <jaakko0606@gmail.com>
2022-11-23 19:46:51 -0500

    yaml and json support

--- zone/create.sh
+++ zone/create.sh
@@ -2,13 +2,13 @@
 
 carburator fn echo info "Invoking Hetzner DNS provider..."
 
-# ATTENTION: --preserve_env (or -k) flag forwards domain env from here to provisioner.
+# ATTENTION: --preserve-env (or -k) flag forwards domain env from here to provisioner.
 # Check if provisioner exists and gives OK response.
 carburator provisioner request \
     create \
     zone \
     --provider "$PROVIDER_NAME" \
     --provisioner "$PROVISIONER_NAME" \
-    --preserve_env || exit 120
+    --preserve-env || exit 120
 
 carburator fn echo success "Hetzner DNS zone for $DOMAIN_FQDN created."
